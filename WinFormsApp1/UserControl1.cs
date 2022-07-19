44996a5e4cc9eb2d8ac3ddff5c7955de58370a28
wanan12138 <89179905+wanan12138@users.noreply.github.com>
2022-07-20 01:34:19 +0800

    服务器一管理

---
+++ WinFormsApp1/UserControl1.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace WinFormsApp1;
+
+public partial class UserControl1 : UserControl
+{
+    public UserControl1()
+    {
+        InitializeComponent();
+    }
+}
