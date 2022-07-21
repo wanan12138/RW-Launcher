d507d63747134ce24e3619bc5c7cdafbc7c3683f
wanan12138 <89179905+wanan12138@users.noreply.github.com>
2022-07-21 11:53:31 +0800

    服务器二

---
+++ WinFormsApp1/UserControl2.Designer.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+
+namespace WinFormsApp1;
+
+partial class UserControl2
+{
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.label1 = new System.Windows.Forms.Label();
+        this.SuspendLayout();
+        //
+        this.label1.Location = new System.Drawing.Point(74, 121);
+        this.label1.Name = "label1";
+        this.label1.Size = new System.Drawing.Size(100, 23);
+        this.label1.TabIndex = 0;
+        this.label1.Text = "芜湖";
+        //
+        components = new System.ComponentModel.Container();
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.Name = "UserControl1";
+        this.Size = new System.Drawing.Size(700,520);
+        this.BackColor = Color.Transparent;
+        this.Controls.Add(this.label1);
+        this.ResumeLayout(false);
+    }
+    private System.Windows.Forms.Label label1;
+    #endregion
+}
---
+++ WinFormsApp1/UserControl2.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace WinFormsApp1;
+
+public partial class UserControl2 : UserControl
+{
+    public UserControl2()
+    {
+        InitializeComponent();
+    }
+}
